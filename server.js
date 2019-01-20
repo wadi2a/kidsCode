a52d430fdc569e6c776f205e5dcd22631de8af91
wadi2a <ben.aissa.ouadie@gmail.com>
2019-01-20 22:10:17 +0100

    installation mongoDB definition base, creation de la route user

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 //Définition des modules
-const express = require("express"); 
-const mongoose = require("mongoose"); 
+const express = require("express");
+const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
+
+//Connexion à la base de donnée
+mongoose.connect('mongodb://127.0.0.1:27017/?gssapiServiceName=mongodb').then(() => {
+    console.log('Connected to mongoDB')
+}).catch(e => {
+    console.log('Error while DB connecting');
+    console.log(e);
+});
+
 
 //On définit notre objet express nommé app
 const app = express();
@@ -22,10 +31,12 @@ app.use(function (req, res, next) {
     next();
 });
 
-//On définit la route Hello
-app.get('/hello',function(req,res){
-    res.json("Hello World")
-})
+//Définition du routeur
+var router = express.Router();
+app.use('/user', router);
+require(__dirname + '/controllers/userController')(router);
+
+
 
 //Définition et mise en place du port d'écoute
 var port = 8000;
